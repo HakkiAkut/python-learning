680604e171fc67a31225793153883752e2c89798
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-01-27 17:58:36 +0300

    lambda

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # python-learning
 
 day1 - basic syntax, data types, string methods, list, tuple, dictionary, sets, operators, if else, loops
-day2 - methods, functions, scope(global, local)
+
+day2 - methods, functions, scope(global, local), lambda, map & filter
--- day2/function.py
+++ day2/function.py
@@ -22,3 +22,22 @@ def charNum(word):
     return { letter: word.count(letter) for letter in word }
 
 print(charNum("deneme"))
+
+def square(a): return a**2
+print(square(4))
+num=range(5)
+# map
+res=list(map(square,num)) 
+
+# lambda
+res2= list(map(lambda num: num**2,range(6)))
+square2 =lambda num: num**2
+res3= list(map(square2,range(3)))
+print(res)
+for item in res2:
+    print(item)
+print(res3)
+
+# filter
+even=lambda num: num%2==0
+print(list(filter(even,range(10))))
