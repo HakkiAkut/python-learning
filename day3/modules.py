2b1e3c9caebaf523e25426222f19b3a2593b6a3c
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-01-28 13:17:55 +0300

    modules

---
+++ day3/modules.py
@@ -0,0 +1,12 @@
+# pip install <package-name> command for installing libs 
+import math as mt
+# from math import * -> will let us use without imported file name(floor() rather than math.floor())
+# from math import factoriel, sqrt ->  also we can import only specified methods
+'''
+value = dir(math) methods
+print(value)
+value = help(math) explenations
+print(value)
+'''
+print(mt.floor(5.9))
+print(mt.sqrt(23))
