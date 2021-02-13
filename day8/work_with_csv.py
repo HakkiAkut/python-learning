5f8882958f5224d2f6ecf74d11ea782644884afe
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-02-13 22:16:59 +0300

    writing csv file

--- day8/work_with_csv.py
+++ day8/work_with_csv.py
@@ -4,14 +4,20 @@ with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
     #print(file.read()) #read all
     csvfile=csv.reader(file)
     print("movies in 1994")
-    for p in csvfile:
-        if p[2]=="1994":
-            print(f"movie name:{p[1]}, movie rating:{p[6]}")
+    with open("day8/movies.csv","w",encoding='UTF-8',newline='') as file: # w/o newline, blank lines appears
+        csvfile1=csv.writer(file)
+        csvfile1.writerow(["movie_name","movie_rating"])
+        for p in csvfile:
+            if p[2]=="1994":
+                print(f"movie name:{p[1]}, movie rating:{p[6]}")
+                csvfile1.writerow([p[1],p[6]])
+    
 
 # read as dictionary
 with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
-    csvfile1=csv.DictReader(file)
-    for p in csvfile1:
+    csvfile2=csv.DictReader(file) #delimeter="|" parses with "|" rather than ","
+    for p in csvfile2:
         if p['Released_Year']=="1994":
             print(p["Series_Title"])
+
 
