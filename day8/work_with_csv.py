287280c6e2ec892cae4f0f8be11a9a314331375e
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-02-13 22:48:09 +0300

    DictWriter append

--- day8/work_with_csv.py
+++ day8/work_with_csv.py
@@ -5,7 +5,7 @@ with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
     csvfile=csv.reader(file)
     print("movies in 1994")
     # csv writing
-    with open("day8/movies.csv","w",encoding='UTF-8',newline='') as file: # w/o newline, blank lines appears
+    with open("day8/movies1994.csv","w",encoding='UTF-8',newline='') as file: # w/o newline, blank lines appears
         csvfile1=csv.writer(file)
         csvfile1.writerow(["released_year","movie_name","movie_rating"])
         for p in csvfile:
@@ -17,7 +17,8 @@ with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
 # read as dictionary
 with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
     csvfile2=csv.DictReader(file) #delimeter="|" parses with "|" rather than ","
-    with open("day8/movies1.csv","w",encoding='UTF-8',newline='') as file:
+    # csv dict writer
+    with open("day8/movies1995-6.csv","w",encoding='UTF-8',newline='') as file:
         csvfile3=csv.DictWriter(file,["Released_Year","Series_Title","IMDB_Rating"])
         csvfile3.writeheader()
         for p in csvfile2:
@@ -26,5 +27,15 @@ with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
                 csvfile3.writerow({"Released_Year":p['Released_Year'],
                 "Series_Title":p["Series_Title"],
                 "IMDB_Rating":p["IMDB_Rating"]})
-
+with open("day8/imdb_top_1000.csv", "r", encoding='UTF-8') as file:
+    csvfile2=csv.DictReader(file) 
+    # if we already add header then open as append rather than write
+    with open("day8/movies1995-6.csv","a",encoding='UTF-8',newline='') as file:
+        csvfile4=csv.DictWriter(file,["Released_Year","Series_Title","IMDB_Rating"])
+        for p in csvfile2:
+            if p['Released_Year']=="1996":
+                print(p["Series_Title"])
+                csvfile4.writerow({"Released_Year":p['Released_Year'],
+                "Series_Title":p["Series_Title"],
+                "IMDB_Rating":p["IMDB_Rating"]})
 
