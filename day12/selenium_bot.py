2368bdc4c5ee05f6991f1e7779352767dee6dd4b
HakkiAkut <32385870+HakkiAkut@users.noreply.github.com>
2021-02-22 23:36:46 +0300

    selenium css selector

--- day12/selenium_bot.py
+++ day12/selenium_bot.py
@@ -6,7 +6,6 @@ driver = webdriver.Chrome(driver_path)
 driver.get("https://www.github.com/HakkiAkut")
 driver.maximize_window()
 driver.save_screenshot("day12/ss.png")
-time.sleep(2)
 print(driver.title)
 #driver.back() returns previous page
 driver.get("https://github.com/HakkiAkut/python-learning")
@@ -14,13 +13,11 @@ driver.get("https://github.com/HakkiAkut/python-learning")
 # doing it with xpath is better imo because we can search for multiple classes
 h1 = driver.find_element_by_xpath("//*[@class=' d-flex flex-wrap flex-items-center break-word f3 text-normal']") 
 print(h1.text)
-time.sleep(1)
 driver.get("https://www.youtube.com")
 video_title= driver.find_element_by_id("video-title") # will get first video title
 print(video_title.text) # video title
 video_channel= driver.find_element_by_xpath("//*[@id='metadata']/div/ytd-channel-name/div/div/yt-formatted-string/a")
 print(video_channel.text) # video channel name
-
 
 videos=driver.find_elements_by_id("video-title")
 channels=driver.find_elements_by_xpath("//*[@id='metadata']/div/ytd-channel-name/div/div/yt-formatted-string/a")
@@ -31,4 +28,14 @@ print(channels)
 for x in videos:
     print(x.text)
 
+py=driver.get("https://www.python.org")
+
+py_names=driver.find_elements_by_id(".blog-widget li a")
+py_times=driver.find_elements_by_css_selector(".blog-widget time")
+
+for i in py_times:
+    print(i.text)
+
+for x in py_names:
+    print(x.text)
 driver.close()
